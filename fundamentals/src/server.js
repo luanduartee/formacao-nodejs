ea9106ed077ade92c766bb9f468ced540f1e282b
lduarte <lduarte@brasal.com.br>
2025-04-05 12:05:15 -0300

    feat: receiving external requests

--- fundamentals/src/server.js
+++ fundamentals/src/server.js
@@ -2,19 +2,34 @@ import http from 'http';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req;
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
+
+    console.log(req.body)
 
     if (method === 'GET' && url === '/users') {
         return res.setHeader('Content-Type', 'application/json').end(JSON.stringify(users));
     }
 
     if (method === 'POST' && url === '/users') {
+        const { name, age, email } = req.body
         users.push({
             id: 1,
-            name: "Josh Beauchamp",
-            age: 25,
-            profession: "Dancer"
+            name,
+            age,
+            email,
         })
 
         return res.writeHead(201).end();
